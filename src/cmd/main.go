117de0078c142eb0ca5aeb7168713e815a6a887d
yuucu <yuucu.sub@gmail.com>
2022-12-05 01:02:33 +0900

    fix

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,45 +1,7 @@
 package main
 
-import (
-	"os"
-	"path/filepath"
-
-	"cuelang.org/go/cue"
-	"cuelang.org/go/cue/load"
-	"cuelang.org/go/encoding/gocode"
-)
-
-const (
-	cueFilePath = "./sample2/user.cue"
-
-	module      = "github.com/sanposhiho/cue-sample/go/sample2"
-	dir         = "./sample2/"
-	destination = "./sample2/cue_gen.go"
-)
+import "fmt"
 
 func main() {
-	cwd, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-
-	dir := filepath.Join(cwd, dir)
-	inst := cue.Build(load.Instances([]string{module}, &load.Config{
-		Dir:        dir,
-		ModuleRoot: dir,
-		Module:     module,
-	}))[0]
-	if err := inst.Err; err != nil {
-		panic(err)
-	}
-
-	b, err := gocode.Generate(dir, inst, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	err = io.WriteFile(destination, b, 0644)
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("hello")
 }
